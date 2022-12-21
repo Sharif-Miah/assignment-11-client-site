2c5436c22adbf8d5a4eca2228dc159aaacb53acc
farhan-sharif <farhansharif1410@gmail.com>
2022-12-21 04:57:28 -0800

    front some error added

--- src/Auth/firebase.config.js
+++ src/Auth/firebase.config.js
@@ -5,14 +5,42 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.REACT_APP_appId
+    // apiKey: process.env.REACT_APP_apiKey,
+    // authDomain: process.env.REACT_APP_authDomain,
+    // projectId: process.env.REACT_APP_projectId,
+    // storageBucket: process.env.REACT_APP_storageBucket,
+    // messagingSenderId: process.env.messagingSenderId,
+    // appId: process.env.REACT_APP_appId
+
+     apiKey: "AIzaSyA-j1tojqUuFpsCygny9mCVz4xPOMGSUCE",
+  authDomain: "doc-care-api-key-new.firebaseapp.com",
+  projectId: "doc-care-api-key-new",
+  storageBucket: "doc-care-api-key-new.appspot.com",
+  messagingSenderId: "57450527920",
+  appId: "1:57450527920:web:b80075fe074d4778d24f75",
+  measurementId: "G-V317KMZNVX"
+    
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+ 
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
